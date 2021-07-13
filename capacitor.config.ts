1607bb79b940c9a4e3c73d463487e25538d7232b
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-07-13 18:28:31 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react1',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
